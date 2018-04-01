fbaa087cae658e629a212053e15d786091a04fc6
Fer Osorio <fer.osorio@live.com>
2018-04-01 13:07:47 -0700

    Core initial create model

--- core/source/Models/Tournament.cs
+++ core/source/Models/Tournament.cs
@@ -45,4 +45,61 @@ namespace FantasyWorldCup.Core.Models
 
         public string Name { get; set; }
     }
+
+    public class Group
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Tournament Tournament { get; set; }
+
+        public User Owner { get; set; }
+
+        public List<UserGroup> UserGroups { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+
+        public Team SupportedTeam { get; set; }
+
+        public List<UserGroup> UserGroups { get; set; }
+    }
+
+    public class UserGroup
+    {
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public int GroupId { get; set; }
+
+        public Group Group { get; set; }
+    }
+    
+
+    public enum Forecast
+    {
+        Tie,
+        LocalWin,
+        VisitorWin
+    }
+
+    public class MatchForecast
+    {
+        public int Id { get; set; }
+        
+        public User User { get; set; }
+        
+        public Match Match { get; set; }
+
+        public Forecast Forecast { get; set; }
+
+        public Group UserGroup { get; set; }
+
+        public int TeamAScore { get; set; } = 0;
+
+        public int TeamBScore { get; set; } = 0;
+    }
 }
--- core/source/Models/TournamentContext.cs
+++ core/source/Models/TournamentContext.cs
@@ -37,13 +37,65 @@ namespace FantasyWorldCup.Core.Models
                 .HasOne(p => p.Tournament)
                 .WithMany(p => p.Matches)
                 .HasForeignKey("TournamentId");
+
+            modelBuilder.Entity<Group>().Property<int>("TournamentId").IsRequired();
+            modelBuilder.Entity<Group>().Property<string>("OwnerId").IsRequired();
+
+            modelBuilder
+                .Entity<Group>()
+                .HasOne(p => p.Tournament)
+                .WithMany()
+                .HasForeignKey("TournamentId");
+
+            modelBuilder.Entity<Group>().HasMany(p => p.UserGroups);
+            modelBuilder.Entity<Group>().HasOne(p => p.Owner).WithMany().HasForeignKey("OwnerId");
+
+            modelBuilder.Entity<MatchForecast>()
+                .HasAlternateKey(p => p.Id);
+
+            modelBuilder.Entity<MatchForecast>().Property<int>("MatchId").IsRequired();
+            modelBuilder.Entity<MatchForecast>().Property<string>("UserId").IsRequired();
+            modelBuilder.Entity<MatchForecast>().Property<int>("UserGroupId").IsRequired();
+
+            modelBuilder.Entity<MatchForecast>()
+                .HasOne(p => p.Match)
+                .WithMany()
+                .HasForeignKey("MatchId");
+
+            modelBuilder.Entity<MatchForecast>()
+                .HasOne(p => p.User)
+                .WithMany()
+                .HasForeignKey("UserId");
+
+            modelBuilder.Entity<MatchForecast>()
+                .HasOne(p => p.UserGroup)
+                .WithMany()
+                .HasForeignKey("UserGroupId");
+
+            modelBuilder.Entity<User>().HasKey(p => p.Id);
+            modelBuilder.Entity<User>().Property<string>("TeamId").IsRequired();
+            modelBuilder.Entity<User>().HasOne(p => p.SupportedTeam).WithMany().HasForeignKey("TeamId");
+
+
+            modelBuilder.Entity<UserGroup>().HasKey(p => new { p.GroupId, p.UserId });
+
+            modelBuilder.Entity<UserGroup>()
+                .HasOne(p => p.Group)
+                .WithMany(p => p.UserGroups)
+                .HasForeignKey(p => p.GroupId);
+
+            modelBuilder.Entity<UserGroup>()
+                .HasOne(p => p.User)
+                .WithMany(p => p.UserGroups)
+                .HasForeignKey(p => p.UserId);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseInMemoryDatabase("InMemoryTournamentDb");
+                // optionsBuilder.UseInMemoryDatabase("InMemoryTournamentDb");
+                optionsBuilder.UseSqlite("DataSource=core.db");
             }
         }
 
